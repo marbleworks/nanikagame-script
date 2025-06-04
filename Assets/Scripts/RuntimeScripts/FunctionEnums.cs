f19d3d5864073c6acfcacf1a944fb765ff8dfbcf
Marble <59565280+marbleworks@users.noreply.github.com>
2025-06-05 04:17:27 +0900

    Use enums for function names

---
+++ Assets/Scripts/RuntimeScripts/FunctionEnums.cs
@@ -0,0 +1,25 @@
+using System;
+
+namespace RuntimeScripting
+{
+    /// <summary>
+    /// Functions returning integer values.
+    /// </summary>
+    public enum FunctionInt
+    {
+        HpMin,
+        ComboCount,
+        Shield,
+        NanikaCount,
+        UseResource,
+        NotDebuffed
+    }
+
+    /// <summary>
+    /// Functions returning floating point values.
+    /// </summary>
+    public enum FunctionFloat
+    {
+        Interval
+    }
+}
