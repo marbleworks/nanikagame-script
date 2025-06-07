5db5cda63386a00292a5c5dc17b9423e3b989bef
Marble <mrmelonman1222@gmail.com>
2025-06-08 05:14:32 +0900

    Refactor GameLogic and ActionParameter for cleaner code structure
    Reorganized methods in GameLogic into logical regions, improved code readability, and extracted void function execution to a helper method. Removed unused fields in ActionParameter and optimized parsing logic for function arguments. These changes enhance maintainability without altering functionality.

--- Assets/Scripts/RuntimeScripts/ActionParameter.cs
+++ Assets/Scripts/RuntimeScripts/ActionParameter.cs
@@ -7,15 +7,5 @@ namespace RuntimeScripting
     {
         public string FunctionName;
         public System.Collections.Generic.List<string> Args = new System.Collections.Generic.List<string>();
-
-        /// <summary>
-        /// Result from executing an integer-returning function.
-        /// </summary>
-        public int? IntResult;
-
-        /// <summary>
-        /// Result from executing a float-returning function.
-        /// </summary>
-        public float? FloatResult;
     }
 }
--- Assets/Scripts/RuntimeScripts/GameLogic.cs
+++ Assets/Scripts/RuntimeScripts/GameLogic.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
@@ -10,14 +11,17 @@ namespace RuntimeScripting
     /// </summary>
     public class GameLogic
     {
-        public int HpMin() => 100;
-        public int ComboCount()
-        {
-            _comboCount++;
-            return _comboCount;
-        }
+        #region Fields
+        private int _comboCount;
+        private int _resourceCount = 1;
+        #endregion
 
-        private int _comboCount = 0;
+        #region Public Functions
+
+        public int HpMin() => 100;
+
+        public int ComboCount() => ++_comboCount;
+
         public int Shield() => 0;
 
         public bool UseResource(string id, int value)
@@ -27,62 +31,59 @@ namespace RuntimeScripting
                 Debug.Log($"UseResource {_resourceCount} < {value}");
                 return false;
             }
-            Debug.Log($"UseResource {_resourceCount} -> {_resourceCount - value}");
             _resourceCount -= value;
+            Debug.Log($"UseResource {id}: {_resourceCount}");
             return true;
         }
 
         public int ResourceCount(string spec) => _resourceCount;
-        private int _resourceCount = 1;
+
         public void AddResource(int value) => _resourceCount += value;
+
         public int NanikaCount(string spec) => 2;
-        public bool NotDebuffed(string target) => true;
         
-        public float Interval(float value) => Random.Range(0.1f, value);
+        public float Interval(float max) => Random.Range(0.1f, max);
         
         public int RandomInt(int min, int max) => Random.Range(min, max);
         
         public float Double(float value) => value * 2f;
+        #endregion
 
+        #region Evaluate Functions
         public int EvaluateFunctionInt(FunctionInt func, string[] args)
         {
             switch (func)
             {
-                case FunctionInt.HpMin: return HpMin();
-                case FunctionInt.ComboCount: return ComboCount();
-                case FunctionInt.Shield: return Shield();
-                case FunctionInt.NanikaCount: return NanikaCount(args.Length > 0 ? args[0] : string.Empty);
-                case FunctionInt.ResourceCount: return ResourceCount(args.Length > 0 ? args[0] : string.Empty);
-                case FunctionInt.UseResource: return UseResource(args[0], int.Parse(args[1])) ? 1 : 0;
-                default: return 0;
+                case FunctionInt.HpMin:
+                    return HpMin();
+                case FunctionInt.ComboCount:
+                    return ComboCount();
+                case FunctionInt.Shield:
+                    return Shield();
+                case FunctionInt.NanikaCount:
+                    return NanikaCount(args.FirstOrDefault() ?? string.Empty);
+                case FunctionInt.ResourceCount:
+                    return ResourceCount(args.FirstOrDefault() ?? string.Empty);
+                case FunctionInt.UseResource:
+                    return UseResource(args[0], int.Parse(args[1])) ? 1 : 0;
+                default:
+                    return 0;
             }
         }
 
         public int EvaluateFunctionInt(string func, string[] args)
-        {
-            if (Enum.TryParse(func, out FunctionInt f))
-                return EvaluateFunctionInt(f, args);
-            return 0;
-        }
+            => Enum.TryParse(func, out FunctionInt f)
+                ? EvaluateFunctionInt(f, args)
+                : 0;
 
-        /// <summary>
-        /// Evaluates a function that returns a floating point value.
-        /// </summary>
-        /// <param name="func">Function name.</param>
-        /// <param name="args">Arguments passed from the script.</param>
-        /// <returns>Function result as float.</returns>
         public float EvaluateFunctionFloat(FunctionFloat func, string[] args)
         {
             switch (func)
             {
-                case FunctionFloat.Interval:
-                    if (args.Length > 0 && float.TryParse(args[0], out float v))
-                        return Interval(v);
-                    return 0f;
-                case FunctionFloat.Double:
-                    if (args.Length > 0 && float.TryParse(args[0], out float v1))
-                        return Double(v1);
-                    return 0f;
+                case FunctionFloat.Interval when args.Length > 0 && float.TryParse(args[0], out var v):
+                    return Interval(v);
+                case FunctionFloat.Double when args.Length > 0 && float.TryParse(args[0], out var d):
+                    return Double(d);
                 default:
                     return 0f;
             }
@@ -90,55 +91,37 @@ namespace RuntimeScripting
 
         public float EvaluateFunctionFloat(string func, string[] args)
         {
-            if (Enum.TryParse(func, out FunctionFloat f))
-                return EvaluateFunctionFloat(f, args);
+            if (Enum.TryParse(func, out FunctionFloat ff))
+                return EvaluateFunctionFloat(ff, args);
             if (Enum.TryParse(func, out FunctionInt fi))
                 return EvaluateFunctionInt(fi, args);
             return 0f;
         }
+        #endregion
 
-        // Action methods used by RuntimeTextScriptController
-        public void Attack(int value)
-        {
-            // Debug.Log($"{ResourceCount}");
-            Debug.Log($"Attack {value}");
-        }
+        #region Action Methods
+        public void Attack(int value) => Debug.Log($"Attack {value}");
 
         public void AddPlayerEffect(string targets, string effectId, int value)
-        {
-            Debug.Log($"Add effect {effectId} {value} to {targets}");
-        }
+            => Debug.Log($"Add effect {effectId} {value} to {targets}");
 
         public void AddPlayerEffectFor(string targets, string effectId, int value, int duration)
-        {
-            Debug.Log($"Add effect {effectId} {value} for {duration} to {targets}");
-        }
+            => Debug.Log($"Add effect {effectId} {value} for {duration} to {targets}");
 
         public void RemoveRandomDebuffPlayerEffect(string targets, int count)
-        {
-            Debug.Log($"Remove {count} debuffs from {targets}");
-        }
+            => Debug.Log($"Remove {count} debuffs from {targets}");
 
         public void AddMaxHp(string targets, int value)
-        {
-            Debug.Log($"Add max hp {value} to {targets}");
-        }
+            => Debug.Log($"Add max hp {value} to {targets}");
 
         public void SetNanikaEffectFor(string targets, string effectId, int value)
-        {
-            Debug.Log($"Set nanika effect {effectId} {value} for {targets}");
-        }
+            => Debug.Log($"Set nanika effect {effectId} {value} for {targets}");
 
         public void SpawnNanika(string targets, string nanikaId, int spawnPosId)
-        {
-            Debug.Log($"Spawn nanika {nanikaId} at {spawnPosId} for {targets}");
-        }
+            => Debug.Log($"Spawn nanika {nanikaId} at {spawnPosId} for {targets}");
+        #endregion
 
-        /// <summary>
-        /// Creates an executable parameter object from a parsed action.
-        /// </summary>
-        /// <param name="pa">Parsed action.</param>
-        /// <returns>Parameter instance.</returns>
+        #region Execution
         internal ActionParameter CreateParameter(ParsedAction pa)
         {
             var param = new ActionParameter
@@ -149,73 +132,58 @@ namespace RuntimeScripting
             return param;
         }
 
-        /// <summary>
-        /// Executes a pre-parsed action.
-        /// </summary>
-        /// <param name="param">Action parameter.</param>
         internal void ExecuteAction(ActionParameter param)
         {
             if (Enum.TryParse(param.FunctionName, out FunctionVoid fv))
             {
-                switch (fv)
-                {
-                    case FunctionVoid.Attack:
-                        if (param.Args.Count > 0)
-                            Attack(ParseIntArg(param.Args[0]));
-                        break;
-                    case FunctionVoid.AddPlayerEffect:
-                        if (param.Args.Count > 2)
-                            AddPlayerEffect(param.Args[0], param.Args[1], ParseIntArg(param.Args[2]));
-                        break;
-                    case FunctionVoid.AddPlayerEffectFor:
-                        if (param.Args.Count > 3)
-                            AddPlayerEffectFor(param.Args[0], param.Args[1], ParseIntArg(param.Args[2]), ParseIntArg(param.Args[3]));
-                        break;
-                    case FunctionVoid.RemoveRandomDebuffPlayerEffect:
-                        if (param.Args.Count > 1)
-                            RemoveRandomDebuffPlayerEffect(param.Args[0], ParseIntArg(param.Args[1]));
-                        break;
-                    case FunctionVoid.AddMaxHp:
-                        if (param.Args.Count > 1)
-                            AddMaxHp(param.Args[0], ParseIntArg(param.Args[1]));
-                        break;
-                    case FunctionVoid.SetNanikaEffectFor:
-                        if (param.Args.Count > 2)
-                            SetNanikaEffectFor(param.Args[0], param.Args[1], ParseIntArg(param.Args[2]));
-                        break;
-                    case FunctionVoid.SpawnNanika:
-                        if (param.Args.Count > 2)
-                            SpawnNanika(param.Args[0], param.Args[1], ParseIntArg(param.Args[2]));
-                        break;
-                }
-            }
-            else if (Enum.TryParse(param.FunctionName, out FunctionInt fi))
-            {
-                param.IntResult = EvaluateFunctionInt(fi, param.Args.ToArray());
-            }
-            else if (Enum.TryParse(param.FunctionName, out FunctionFloat ff))
-            {
-                param.FloatResult = EvaluateFunctionFloat(ff, param.Args.ToArray());
-            }
-            else
-            {
-                param.FloatResult = EvaluateFunctionFloat(param.FunctionName, param.Args.ToArray());
+                ExecuteVoidFunction(fv, param);
             }
         }
 
-        private int ParseIntArg(string arg)
+        private void ExecuteVoidFunction(FunctionVoid fv, ActionParameter param)
         {
-            if (int.TryParse(arg, out var value))
-                return value;
-
-            return IntExpressionEvaluator.Evaluate(arg, this);
+            switch (fv)
+            {
+                case FunctionVoid.Attack:
+                    if (param.Args.Count > 0)
+                        Attack(ParseIntArg(param.Args[0]));
+                    break;
+                case FunctionVoid.AddPlayerEffect:
+                    if (param.Args.Count > 2)
+                        AddPlayerEffect(param.Args[0], param.Args[1], ParseIntArg(param.Args[2]));
+                    break;
+                case FunctionVoid.AddPlayerEffectFor:
+                    if (param.Args.Count > 3)
+                        AddPlayerEffectFor(param.Args[0], param.Args[1], ParseIntArg(param.Args[2]), ParseIntArg(param.Args[3]));
+                    break;
+                case FunctionVoid.RemoveRandomDebuffPlayerEffect:
+                    if (param.Args.Count > 1)
+                        RemoveRandomDebuffPlayerEffect(param.Args[0], ParseIntArg(param.Args[1]));
+                    break;
+                case FunctionVoid.AddMaxHp:
+                    if (param.Args.Count > 1)
+                        AddMaxHp(param.Args[0], ParseIntArg(param.Args[1]));
+                    break;
+                case FunctionVoid.SetNanikaEffectFor:
+                    if (param.Args.Count > 2)
+                        SetNanikaEffectFor(param.Args[0], param.Args[1], ParseIntArg(param.Args[2]));
+                    break;
+                case FunctionVoid.SpawnNanika:
+                    if (param.Args.Count > 2)
+                        SpawnNanika(param.Args[0], param.Args[1], ParseIntArg(param.Args[2]));
+                    break;
+            }
         }
-    }
-    
+        #endregion
 
-    /// <summary>
-    /// Functions that perform actions and return no value.
-    /// </summary>
+        #region Helpers
+        private int ParseIntArg(string arg)
+            => int.TryParse(arg, out var val)
+                ? val
+                : IntExpressionEvaluator.Evaluate(arg, this);
+        #endregion
+    }
+
     public enum FunctionVoid
     {
         Attack,
@@ -227,9 +195,6 @@ namespace RuntimeScripting
         SpawnNanika
     }
     
-    /// <summary>
-    /// Functions returning integer values.
-    /// </summary>
     public enum FunctionInt
     {
         HpMin,
@@ -237,12 +202,9 @@ namespace RuntimeScripting
         Shield,
         NanikaCount,
         ResourceCount,
-        UseResource,
+        UseResource
     }
 
-    /// <summary>
-    /// Functions returning floating point values.
-    /// </summary>
     public enum FunctionFloat
     {
         Interval,
